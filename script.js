ca1a4d6397a42f7e53cb5e5772c5c4ac8f9151bb
Evan Geiss <egeiss@umich.edu>
2023-09-27 04:22:58 -0400

    add other pages

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Function to fetch and display GitHub repositories
 async function displayGitHubRepositories() {
     const username = 'evan-geiss'; // Replace with Evan's GitHub username
     const url = `https://api.github.com/users/${username}/repos`;
@@ -10,15 +9,40 @@ async function displayGitHubRepositories() {
         if (response.ok) {
             const repositoriesContainer = document.getElementById('github-repositories');
 
-            // Loop through the repositories and create HTML elements to display them
-            data.forEach(repository => {
+            // Loop through the repositories
+            data.forEach(async (repository) => {
+                // Create a container for each repository
+                const repoContainer = document.createElement('div');
+                repoContainer.classList.add('repository-container');
+
+                // Create a link to the repository
                 const repoLink = document.createElement('a');
                 repoLink.href = repository.html_url;
                 repoLink.textContent = repository.name;
                 repoLink.target = '_blank';
+                repoContainer.appendChild(repoLink);
 
-                repositoriesContainer.appendChild(repoLink);
-                repositoriesContainer.appendChild(document.createElement('br'));
+                // Create an image for the repository's avatar
+                const avatarImg = document.createElement('img');
+                avatarImg.src = repository.owner.avatar_url;
+                avatarImg.alt = `${repository.owner.login}'s Avatar`;
+                repoContainer.appendChild(avatarImg);
+
+                // Fetch the README content
+                const readmeUrl = `${repository.url}/readme`;
+                const readmeResponse = await fetch(readmeUrl);
+                const readmeData = await readmeResponse.json();
+
+                if (readmeResponse.ok) {
+                    // Create a div to display the README content
+                    const readmeDiv = document.createElement('div');
+                    readmeDiv.innerHTML = atob(readmeData.content);
+                    repoContainer.appendChild(readmeDiv);
+                } else {
+                    console.error('Failed to fetch README for', repository.name);
+                }
+
+                repositoriesContainer.appendChild(repoContainer);
             });
         } else {
             console.error('Failed to fetch GitHub repositories:', data.message);
@@ -27,6 +51,15 @@ async function displayGitHubRepositories() {
         console.error('An error occurred while fetching GitHub repositories:', error);
     }
 }
+
+document.querySelectorAll('.nav-link').forEach(item => {
+    item.addEventListener('click', () => {
+        const navbarToggler = document.querySelector('.navbar-toggler');
+        if (navbarToggler.getAttribute('aria-expanded') === 'true') {
+            navbarToggler.click();
+        }
+    });
+});
 
 // Call the function to display GitHub repositories
 displayGitHubRepositories();
