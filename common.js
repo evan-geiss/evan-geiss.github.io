2799e897cd10fc976d0a238506af676d1cd65af7
Evan Geiss <egeiss@umich.edu>
2023-09-27 06:22:36 -0400

    weewweewwe

--- common.js
+++ common.js
@@ -15,6 +15,6 @@ loadExternalScript('https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/p
 loadExternalScript('https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js');
 loadExternalScript('https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js');
 
-loadExternalScript('https://code.jquery.com/jquery-3.5.1.slim.min.js');
-loadExternalScript('https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js');
-loadExternalScript('https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js');
+loadExternalScriptInBody('https://code.jquery.com/jquery-3.5.1.slim.min.js');
+loadExternalScriptInBody('https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js');
+loadExternalScriptInBody('https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js');
