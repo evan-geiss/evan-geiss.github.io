d8513d53e452a48a7a446c0eb35218d6a39fc4bb
Evan Geiss <egeiss@umich.edu>
2023-09-27 05:47:18 -0400

    34343452234523452345

---
+++ common.js
@@ -0,0 +1,10 @@
+function loadExternalScript(src) {
+    var script = document.createElement('script');
+    script.src = src;
+    document.head.appendChild(script);
+}
+
+loadExternalScript('https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js');
+loadExternalScript('https://code.jquery.com/jquery-3.5.1.slim.min.js');
+loadExternalScript('https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js');
+loadExternalScript('https://cdn.jsdelivr.net/npm/@popperjs>core@2.5.4/dist/umd/popper.min.js');
